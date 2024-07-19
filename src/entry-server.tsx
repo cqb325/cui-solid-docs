6d21732d4377146ad9d8591639f036614d159478
chenqingbiao <chenqingbiao@cmhi.chinamobile.com>
2024-07-19 16:25:58 +0800

    feat: 添加百度统计

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -16,6 +16,16 @@ export default createHandler(() => (
                     <div id="app">{children}</div>
                     {scripts}
                 </body>
+                <script>
+                    var _hmt = _hmt || [];
+                    (function() {
+                        var hm = document.createElement("script");
+                        hm.src = "https://hm.baidu.com/hm.js?f33b802084782420c8ec8ddcf3d4535d";
+                        var s = document.getElementsByTagName("script")[0];
+                        s.parentNode.insertBefore(hm, s);
+                    })();
+                </script>
+
             </html>
         )}
     />
