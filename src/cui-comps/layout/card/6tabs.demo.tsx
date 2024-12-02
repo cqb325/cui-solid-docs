9acfea15c80cf53a482cceb3ce5a7828af638d27
chenqingbiao <chenqingbiao@cmhi.chinamobile.com>
2024-12-02 16:32:15 +0800

    fix: 精简f7icons & card例子

--- src/cui-comps/layout/card/6tabs.demo.tsx
+++ src/cui-comps/layout/card/6tabs.demo.tsx
@@ -1,10 +1,10 @@
-import { Card, Tabs, Tab} from "cui-solid"
+import { Card, Tabs, TabPane} from "cui-solid"
 
 export default () => {
     return <Card title='Title' bordered>
         <Tabs>
-            <Tab name="tab1" title="标签一">内容tab1内容tab1</Tab>
-            <Tab name="tab2" title="标签二">内容tab2内容tab2</Tab>
+            <TabPane name="tab1" title="标签一">内容tab1内容tab1</TabPane>
+            <TabPane name="tab2" title="标签二">内容tab2内容tab2</TabPane>
         </Tabs>
     </Card>
 }
